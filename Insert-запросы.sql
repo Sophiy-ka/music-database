5cc245b60d2d836226d1977841830747b14503e5
София Калугина <kaluginas104@gmail.com>
2026-03-02 09:29:43 +0300

    Обновлен Insert-запросы.sql

--- Insert-запросы.sql
+++ Insert-запросы.sql
@@ -23,10 +23,11 @@ insert  into Albums(Albums_ID, Albums_title, Release_year) values
 (2, 'American Idiot', '2004'),
 (3, 'Toxicity', '2001'),
 (4, 'Химера', '2001'),
-(5, 'Victorious', '2019');
+(5, 'Victorious', '2019')
+(6, 'Still Breathing', '2018');
 
 insert  into Albums_Performers(Albums_ID, Performers_ID) values
-(1,4), (2,1), (3,2), (4,3), (5,5);
+(1,4), (2,1), (3,2), (4,3), (5,5), (6,5);
 
 insert  into Tracks(Tracks_ID, Song_title, Duration, Albums_ID) values
 (1, 'Штиль', 329, 4),
@@ -35,7 +36,8 @@ insert  into Tracks(Tracks_ID, Song_title, Duration, Albums_ID) values
 (4, 'Wake Me Up When September Ends', 267, 2),
 (5, 'Boulevard of Broken Dreams', 252, 2),
 (6, 'Два вопроса', 241, 1),
-(7, 'Victorious', 207, 5);
+(7, 'Victorious', 207, 5)
+(8, 'My Demons', 245);
 
 
 insert into Compilations(Compilations_ID, Title, Release_year) values
@@ -47,13 +49,5 @@ insert into Compilations(Compilations_ID, Title, Release_year) values
 insert into Compilations_Tracks(Compilations_ID, Tracks_ID) values
 (1,1), (1,2), (1,3),
 (2,1), (2,6),
-(3,5), (3,1),
-(4,4), (4,5);
-
-
-
-
-
-
-
-
+(3,5), (3,1),(4,7)
+(4,4), (4,5), (3,8);
